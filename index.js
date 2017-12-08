d05918bad464b17d49cf0639d1efca31f691e738
Rollin Metzger <rollin.metzger@gmail.com>
2017-12-08 18:26:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function theBeatlesPlay(arraym, arrayi) {
 function johnLennonFacts(facts) {
   var i = 0
   while (i < facts.length) {
-    facts[i]
+    facts[i] = facts[i] + "!!!"
     i++
   }return facts
 }
